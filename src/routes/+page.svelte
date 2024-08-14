64a276a15d852600111bf6286f2b30455b769a84
Mikhail Katsupko <t63723@gmail.com>
2024-08-14 17:33:40 +0700

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+
+    let isTelegramWebApp: boolean = false;
+    let webAppData: any = null; // Store the Telegram.WebApp object
+
+    onMount(() => {
+        isTelegramWebApp =
+            window.Telegram && window.Telegram.WebApp !== undefined;
+
+        if (isTelegramWebApp) {
+            webAppData = window.Telegram.WebApp;
+        }
+    });
+</script>
+
+{#if !isTelegramWebApp}
+    <p>
+        Telegram.WebApp not found. Make sure this script is running within a
+        Telegram Mini App.
+    </p>
+{:else}
+    <h2>Telegram.WebApp Data:</h2>
+    <pre>{JSON.stringify(webAppData, null, 2)}</pre>
+{/if}
